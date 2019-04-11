711bfaa2606927f0304d7d2922035b63fce9e081
nusrat <nusrathossain52@gmail.com>
2019-04-10 20:47:44 -0400

    first commit

---
+++ src/firstPackage/FirstClass.java
@@ -0,0 +1,16 @@
+package firstPackage;
+
+public class FirstClass {
+	System.out.println("HELLO WORLD");
+	String name = "my Name";
+	String name1="your Name";
+	
+	//Loops
+			//For loop
+			//write value of x 5 times
+			// s = 0 means this is where my condition starts 
+			// s<6 means this is where my condition ends
+			//++ means incremental
+			//-- means decremental
+	
+			for (int s = 0; s<==4;s++ ){
